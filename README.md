49d6cc99238f7a726eb94e0b942eb88b29b752bf
FJvdPol <folkertjanvanderpol@gmail.com>
2018-10-29 16:40:22 +0100

    Rewrote Rijk's api to understand the process myself

---
+++ README.md
@@ -0,0 +1,7 @@
+# Functional Programming
+Currently: way to communicate with the OBA Api
+## install
+clone or download this repository and navigate to it with your console
+```
+npm install
+```
--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-const OBA = require('oba-api')
-
+// const OBA = require('oba-api')
+const convert = require('xml-to-json-promise');
 require('dotenv').config()
 
-const client = new OBA({
+// const axios = require('axios')
+// let parser = require('xml2js').parseString
+
+const config = {
   public: process.env.PUBLIC,
   secret: process.env.SECRET
-})
+}
 
-client.get('search', {
-  q: 'rijk',
-  sort: 'title'
-})
-  .then(res => console.log(res)) // JSON results
-  .catch(err => console.log(err))
+// const client = new OBA({
+//   public: config.public,
+//   secret: config.secret
+// })
+// client.get('search', {
+//   q: 'rijk',
+//   sort: 'title'
+// })
+//   .then(res => JSON.parse(res))
+//   .then(res => console.log(res.aquabrowser.results.result[0]))
+//   .catch(err => console.log(err))
+
+const axios = require('axios');
+var parseString = require('xml2js').parseString;
+
+
+axios.get(`https://zoeken.oba.nl/api/v1/search/?q=boek&authorization=${config.public + ''}`)
+  .then(res => {
+    return convert.xmlDataToJSON(res.data)
+  })
+  .then((err, result) => {
+    err ? console.log(err) : console.log(result)
+  })
+  .catch(error => console.log('error: ', error))
