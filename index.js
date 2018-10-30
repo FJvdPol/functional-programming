8163970e8834b8308284c72897f776eabf8528dc
FJvdPol <folkertjanvanderpol@gmail.com>
2018-10-30 12:33:01 +0100

    Rewrote api to work with different endpoints

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const OBA = require('./oba-api.js')
+const express = require('express')
+
+
 require('dotenv').config()
 
+const port = 8080
+const data = {
+  response: null
+}
 const oba = new OBA({
   key: process.env.PUBLIC
 })
-oba.get('search', {
-  q: 'harry potter',
-  sort: 'title',
-  facet: 'type(book)'
-}).then(res => console.log(res))
+
+oba.get('holdings/root').then(res => {
+  data.response = res
+}).catch(err => console.log('error'))
+
+
+
+
+const app = express()
+
+app.get('/', (req, res) => res.json(data.response.aquabrowser))
+
+app.listen(port)
--- oba-api.js
+++ oba-api.js
@@ -1,18 +1,23 @@
 const axios = require('axios')
 const convert = require('xml-to-json-promise').xmlDataToJSON
-const queryToString = require('query-string').stringify
-
 // Based on the oba api by rijkvanzanten
 
 class API {
   constructor(options) {
     // set authentication key dependent on options passed in new OBA instance
     this.key = options.key
+    console.log(this.stringify({name: 'folkert', value: 'true', hello: 'hello'}));
+  }
+  stringify(object) {
+    const keys = Object.keys(object)
+    const values = Object.values(object)
+    return keys.map((key, i) => `&${key}=${values[i]}`).join('')
   }
   // endpoint: search | details | refine | schema | availability | holdings
   // params: query parameters, check api docs for possibilities
-  get(endpoint, params) {
-    const url = `https://zoeken.oba.nl/api/v1/${endpoint}/?${queryToString(params)}&authorization=${this.key}`
+  get(endpoint, params = {}) {
+    const url = `https://zoeken.oba.nl/api/v1/${endpoint}/?authorization=${this.key}${this.stringify(params)}`
+    console.log(url);
     return new Promise((resolve, reject) => {
       axios.get(url)
         .then(res => convert(res.data))
