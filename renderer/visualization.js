3ecbb4d88e0bb21d58cbd0d16f7fe1b73176d327
FJvdPol <folkertjanvanderpol@gmail.com>
2018-11-08 16:35:22 +0100

    Fixed y position of each language and added color range

--- renderer/visualization.js
+++ renderer/visualization.js
@@ -1,6 +1,6 @@
 function countKeyTotals(arr) {
-  return arr.reduce(function(obj, b) {
-    obj[b] = ++obj[b] || 1
+  return arr.reduce((obj, key) => {
+    obj[key] = ++obj[key] || 1
     return obj
   }, {})
 }
@@ -18,39 +18,68 @@ function destructData(data){
   const publication = d3.nest()
     .key(book => book.publication)
     .entries(data)
+  const language = d3.nest()
+      .key(book => book.language)
+      .entries(data)
 
-  const allYears = publication.reduce((total, year) => total.concat(Number(year.key)), [])
-  books.years = allYears
+  books.languages = language.reduce((total, lang) => total.concat(lang.key), [])
 
-  const mostPerYear = publication.map(year => year.values.length)
-  books.most = mostPerYear
+  books.years = publication.reduce((total, year) => total.concat(Number(year.key)), [])
 
-  const booksByLanguage = d3.nest()
-    .key(book => book.language)
-    .entries(data)
+  books.most = publication.map(year => year.values.length)
+
+  const stack = d3.stack()
+    .keys(books.languages)
+    .order(d3.stackOrderNone)
+    .offset(d3.stackOffsetNone)
+
+  books.byYear = publication
+    .map(year => d3.nest()
+        .key(book => book.language)
+        .entries(year.values)
+        .map(lang => ({...lang, total: lang.values.length}))
+        .sort((a,b) => a.key.localeCompare(b.key))
+        .reduce((total, lang, index) => {
+          let base
+          index == 0 ? base = 0 : base = total[index - 1].y[1]
+          return total.concat({...lang, lang: lang.key, year: lang.values[0].publication, y: [base, base + lang.total]})
+        }, [])
+    )
+
+    // .map(year =>
+    //   year.reduce((total, lang) => {
+    //     total[lang.key] = lang.values.length
+    //     return total
+    //   }, {})
+    // )
+    // .map(langArr => ({y: langArr.slice(0,2), year: langArr.data.values[0].publication, lang: langArr.data.key, ...langArr.data}))
+    // .reduce((arr, cur) => arr.concat(cur), [])
 
 
-  books.all = booksByLanguage.map(lang => {
-    return {
-      lang: lang.key,
-      years: d3.nest().key(book => book.publication).entries(lang.values)
-    }
-  })
+  console.log('booksByYear: ',books.byYear)
+
+  books.byLang = d3.nest()
+    .key(book => book.lang)
+    .entries(books.byYear
+      .reduce((total, cur) => total.concat(cur))
+    )
+
   drawData(books)
 }
 
 
 function drawData(books) {
-
   console.log('x domain: ', d3.extent(books.years))
   console.log('y domain: ', d3.max(books.most))
-  console.log(books.all);
+  // console.log(books.byLang);
 
-  const height = 400
-  const width = 400
+
+
+  const height = window.innerHeight / 1.5
+  const width = window.innerWidth
   const barWidth = 30
   const offset = 5
-  const years = 17
+  // const years = books.years.length
 
   const yScale = d3.scaleLinear()
     .domain([0, d3.max(books.most)])
@@ -61,32 +90,47 @@ function drawData(books) {
     .paddingInner(.3)
     .paddingOuter(.3)
 
+  margin = ({top: 10, right: 10, bottom: 20, left: 40})
+  const xAxis = g => g
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale).tickSizeOuter(0))
+    .call(g => g.selectAll(".domain").remove())
+
+  const yAxis = g => g
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(yScale).ticks(null, "s"))
+    .call(g => g.selectAll(".domain").remove())
+
+
+
+  const color = d3.scaleLinear().domain([1, books.byLang.length])
+      .interpolate(d3.interpolateHcl)
+      .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')])
+
+
   const svg = d3.select('body')
     .append('svg')
     .style('background', 'papayawhip')
     .attr('width', width)
     .attr('height', height)
 
-  // console.log(books.all[0].years);
-  // svg.selectAll('rect')
-  //   .data(books.all[0].years).enter().append('rect')
-  //     .attr('fill', 'blue')
-  //     .attr('width', xScale.bandwidth())
-  //     .attr('height', d => yScale(d.values.length))
-  //     .attr('x', d => xScale(Number(d.key)))
-  //     .attr('y', d => height - yScale(d.values.length))
-
-
-  // return
   svg.selectAll('g')
-    .data(books.all).enter().append('g') // every language one group
+    .data(books.byLang).enter().append('g') // every language one group
+    .attr('fill', (d, index) => color(index))
       .selectAll('rect')
-        .data(d => d.years).enter().append('rect') // every year in language one rect
-          .attr('fill', 'blue')
+        .data(d => d.values).enter().append('rect') // every year in language one rect
           .attr('width', xScale.bandwidth())
-          .attr('height', d => yScale(d.values.length))
-          .attr('x', d => xScale(Number(d.key)))
-          .attr('y', (d, index) => {
-            return height - yScale(d.values.length)
+          .attr('height', d => {
+            // console.log(d)
+            return yScale(d.y[1] - d.y[0])
           })
+          .attr('data-lang', d => d.lang)
+          .attr('x', d => xScale(Number(d.year)))
+          .attr('y', d => height - yScale(d.y[1]) )
+
+  svg.append("g")
+      .call(xAxis)
+
+  svg.append("g")
+      .call(yAxis)
 }
